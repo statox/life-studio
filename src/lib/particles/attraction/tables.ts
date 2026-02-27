25fedca27c8c5c4a279ca67e47574f0b3c630b31
statox <me@statox.fr>
2026-02-27 17:36:55 +0100

    Formatting

--- src/lib/particles/attraction/tables.ts
+++ src/lib/particles/attraction/tables.ts
@@ -31,30 +31,10 @@ export const getRandomAttractionTable = (): AttractionTable => {
 
 export const getZeroedAttractionTable = (): AttractionTable => {
     return {
-        white: {
-            white: 0,
-            red: 0,
-            green: 0,
-            blue: 0
-        },
-        red: {
-            white: 0,
-            red: 0,
-            green: 0,
-            blue: 0
-        },
-        green: {
-            white: 0,
-            red: 0,
-            green: 0,
-            blue: 0
-        },
-        blue: {
-            white: 0,
-            red: 0,
-            green: 0,
-            blue: 0
-        }
+        white: { white: 0, red: 0, green: 0, blue: 0 },
+        red: { white: 0, red: 0, green: 0, blue: 0 },
+        green: { white: 0, red: 0, green: 0, blue: 0 },
+        blue: { white: 0, red: 0, green: 0, blue: 0 }
     };
 };
 export const tables: StoredTable[] = [
@@ -62,30 +42,10 @@ export const tables: StoredTable[] = [
         name: 'Negs',
         description: 'Only -1',
         table: {
-            white: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            },
-            red: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            },
-            green: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            },
-            blue: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            }
+            white: { white: -1, red: -1, green: -1, blue: -1 },
+            red: { white: -1, red: -1, green: -1, blue: -1 },
+            green: { white: -1, red: -1, green: -1, blue: -1 },
+            blue: { white: -1, red: -1, green: -1, blue: -1 }
         }
     },
     {
@@ -93,60 +53,20 @@ export const tables: StoredTable[] = [
         description:
             'Some very simple patterns of reds chasing whites in a seemlingly infinite randomness',
         table: {
-            white: {
-                white: -1,
-                red: -1,
-                green: 0,
-                blue: -1
-            },
-            red: {
-                white: 2,
-                red: -1,
-                green: 1,
-                blue: -1
-            },
-            green: {
-                white: 0,
-                red: 0,
-                green: -1,
-                blue: -1
-            },
-            blue: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            }
+            white: { white: -1, red: -1, green: 0, blue: -1 },
+            red: { white: 2, red: -1, green: 1, blue: -1 },
+            green: { white: 0, red: 0, green: -1, blue: -1 },
+            blue: { white: -1, red: -1, green: -1, blue: -1 }
         }
     },
     {
         name: 'No cohesion',
         description: 'Cells of the same color repulse each other',
         table: {
-            white: {
-                white: -1,
-                red: 0,
-                green: 0,
-                blue: 0
-            },
-            red: {
-                white: 0,
-                red: -1,
-                green: 0,
-                blue: 0
-            },
-            green: {
-                white: 0,
-                red: 0,
-                green: -1,
-                blue: 0
-            },
-            blue: {
-                white: 0,
-                red: 0,
-                green: 0,
-                blue: -1
-            }
+            white: { white: -1, red: 0, green: 0, blue: 0 },
+            red: { white: 0, red: -1, green: 0, blue: 0 },
+            green: { white: 0, red: 0, green: -1, blue: 0 },
+            blue: { white: 0, red: 0, green: 0, blue: -1 }
         }
     },
     {
@@ -154,30 +74,10 @@ export const tables: StoredTable[] = [
         description:
             'All the cells repulse each others, resulting in an even repartition of the cells',
         table: {
-            white: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            },
-            red: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            },
-            green: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            },
-            blue: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: -1
-            }
+            white: { white: -1, red: -1, green: -1, blue: -1 },
+            red: { white: -1, red: -1, green: -1, blue: -1 },
+            green: { white: -1, red: -1, green: -1, blue: -1 },
+            blue: { white: -1, red: -1, green: -1, blue: -1 }
         }
     },
     {
@@ -185,30 +85,10 @@ export const tables: StoredTable[] = [
         description:
             'The colors attract each other in a circular manner creating some moving snakes',
         table: {
-            white: {
-                white: -1,
-                red: 1,
-                green: 0,
-                blue: 0
-            },
-            red: {
-                white: 0,
-                red: -1,
-                green: 1,
-                blue: 0
-            },
-            green: {
-                white: 0,
-                red: 0,
-                green: -1,
-                blue: 1
-            },
-            blue: {
-                white: 1,
-                red: 0,
-                green: 0,
-                blue: -1
-            }
+            white: { white: -1, red: 1, green: 0, blue: 0 },
+            red: { white: 0, red: -1, green: 1, blue: 0 },
+            green: { white: 0, red: 0, green: -1, blue: 1 },
+            blue: { white: 1, red: 0, green: 0, blue: -1 }
         }
     },
     {
@@ -216,30 +96,10 @@ export const tables: StoredTable[] = [
         description:
             'The cells regroup in moving structures with white/red head and blue/green tail swimming through the world.',
         table: {
-            white: {
-                white: 0,
-                red: 1,
-                green: -1,
-                blue: -2
-            },
-            red: {
-                white: 2,
-                red: 0,
-                green: 1,
-                blue: -1
-            },
-            green: {
-                white: -2,
-                red: -2,
-                green: -2,
-                blue: 1
-            },
-            blue: {
-                white: 2,
-                red: -1,
-                green: 1,
-                blue: -2
-            }
+            white: { white: 0, red: 1, green: -1, blue: -2 },
+            red: { white: 2, red: 0, green: 1, blue: -1 },
+            green: { white: -2, red: -2, green: -2, blue: 1 },
+            blue: { white: 2, red: -1, green: 1, blue: -2 }
         }
     },
     {
@@ -247,60 +107,20 @@ export const tables: StoredTable[] = [
         description:
             'Create some moving objects mainly drivent by green and red and some living but still objects made by white, blue and red. Sometimes the movers grab still objects.',
         table: {
-            white: {
-                white: 1,
-                red: -1,
-                green: 0,
-                blue: 1
-            },
-            red: {
-                white: 2,
-                red: 1,
-                green: 2,
-                blue: -1
-            },
-            green: {
-                white: -1,
-                red: -1,
-                green: 1,
-                blue: -1
-            },
-            blue: {
-                white: 1,
-                red: 2,
-                green: 0,
-                blue: 0
-            }
+            white: { white: 1, red: -1, green: 0, blue: 1 },
+            red: { white: 2, red: 1, green: 2, blue: -1 },
+            green: { white: -1, red: -1, green: 1, blue: -1 },
+            blue: { white: 1, red: 2, green: 0, blue: 0 }
         }
     },
     {
         name: 'White pushers',
         description: 'Create movers propelled by groups of white cells',
         table: {
-            white: {
-                white: 1,
-                red: 1,
-                green: 2,
-                blue: -1
-            },
-            red: {
-                white: 0,
-                red: -1,
-                green: 2,
-                blue: 2
-            },
-            green: {
-                white: -1,
-                red: 0,
-                green: -1,
-                blue: 2
-            },
-            blue: {
-                white: -1,
-                red: 1,
-                green: 1,
-                blue: -1
-            }
+            white: { white: 1, red: 1, green: 2, blue: -1 },
+            red: { white: 0, red: -1, green: 2, blue: 2 },
+            green: { white: -1, red: 0, green: -1, blue: 2 },
+            blue: { white: -1, red: 1, green: 1, blue: -1 }
         }
     },
     {
@@ -308,30 +128,10 @@ export const tables: StoredTable[] = [
         description:
             'Create large gliders with branches. The cells are ordered white, blue, red green',
         table: {
-            white: {
-                white: 0,
-                red: 1,
-                green: 0,
-                blue: 2
-            },
-            red: {
-                white: 0,
-                red: 1,
-                green: 1,
-                blue: -1
-            },
-            green: {
-                white: -1,
-                red: 1,
-                green: 0,
-                blue: 1
-            },
-            blue: {
-                white: -1,
-                red: 2,
-                green: 2,
-                blue: 2
-            }
+            white: { white: 0, red: 1, green: 0, blue: 2 },
+            red: { white: 0, red: 1, green: 1, blue: -1 },
+            green: { white: -1, red: 1, green: 0, blue: 1 },
+            blue: { white: -1, red: 2, green: 2, blue: 2 }
         }
     },
     {
@@ -339,30 +139,10 @@ export const tables: StoredTable[] = [
         description:
             'Galaxies with blue center, red and white rings turning around evolving in a green goo',
         table: {
-            white: {
-                white: -1,
-                red: 1,
-                green: 1,
-                blue: 1
-            },
-            red: {
-                white: -1,
-                red: -1,
-                green: -1,
-                blue: 2
-            },
-            green: {
-                white: -2,
-                red: -1,
-                green: 0,
-                blue: -1
-            },
-            blue: {
-                white: 1,
-                red: 2,
-                green: -2,
-                blue: 2
-            }
+            white: { white: -1, red: 1, green: 1, blue: 1 },
+            red: { white: -1, red: -1, green: -1, blue: 2 },
+            green: { white: -2, red: -1, green: 0, blue: -1 },
+            blue: { white: 1, red: 2, green: -2, blue: 2 }
         }
     },
     {
