d4564eb77086dae3185a430335759a8fcdc47332
statox <me@statox.fr>
2026-03-06 20:07:33 +0100

    Use base url in links to fix the CI build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
 <p>My experiments with emergent behaviors</p>
 
 <ul>
-    <li><a href="/gameoflife">Game of Life on the GPU</a></li>
-    <li><a href="/reaction-diffusion">Gray-Scott reaction-diffusion model</a></li>
-    <li><a href="/particles-life">Particle life in web workers</a></li>
+    <li><a href="{base}/gameoflife">Game of Life on the GPU</a></li>
+    <li><a href="{base}/reaction-diffusion">Gray-Scott reaction-diffusion model</a></li>
+    <li><a href="{base}/particles-life">Particle life in web workers</a></li>
 </ul>
