a74a8c8a8b598c63b764492af7f205206c1adc84
statox <me@statox.fr>
2026-04-07 18:14:40 +0200

    Rework CSS

---
+++ src/routes/particles-life/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import '$lib/particles/particles.css';
+
+    interface Props {
+        children?: import('svelte').Snippet;
+    }
+
+    let { children }: Props = $props();
+</script>
+
+{@render children?.()}
