93abb609529c9ade2289b7252cb3b0d4b07bdf4d
statox <me@statox.fr>
2026-04-13 13:03:30 +0000

    Deploying to gh-pages from @ statox/life-studio@5fca6f1ac0751e1d6b1a74e195286dfef14df35e 🚀

--- _app/immutable/entry/start.DEcpw7UI.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CcnaBvjJ.js";import{x as t}from"../chunks/Ds-Hw01X.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.DVlAWm9S.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BX5EVxEX.js";import{x as t}from"../chunks/DObJSg7b.js";export{t as load_css,r as start};
